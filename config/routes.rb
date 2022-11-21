b2394f730220bf6698b11bf6ac240a3da89eb109
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-11-21 14:51:48 +0300

    Completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+  get 'students', to: 'students#index'
+  get 'students/grades', to: 'students#grades'
+  get 'students/highest_grade', to: 'students#highest_grade'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
